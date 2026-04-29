becce50c135bb7f3be0be167f2ec5e88d5037bfb
luchinofan22 <s1038294@student.knoxschools.org>
2026-04-29 13:40:33 +0000

    ohhohhhohhh caught in a bad romanceeee

--- app.py
+++ app.py
@@ -83,14 +83,14 @@ def dashboard():
     #     return redirect(url_for("login"))
 
     # #  XTODO: Connect to the database
-    # conn = get_db()
+    conn = get_db()
 
     # #  XTODO: Get all entries that belong to the logged-in user
     # # Example:
-    # entries = conn.execute(
-    #       "SELECT * FROM entries WHERE user=?",
-    #       (session["user"],)
-    #   ).fetchall()
+    entries = conn.execute(
+          "SELECT * FROM entries WHERE user=?",
+          (session["user"],)
+      ).fetchall()
 
     # # TODO: Close the connection
     # conn.close()
